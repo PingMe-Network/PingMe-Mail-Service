7da5e0978ece5160d077cf640a0cd965186e8c12
phatdang <phatdang19052004@gmail.com>
2026-01-18 16:39:16 +0700

    feat(application.properties): cấu hình mail service

---
+++ src/main/java/phatdang/ping_me/PingMeMailServiceApplication.java
@@ -0,0 +1,13 @@
+package phatdang.ping_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingMeMailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingMeMailServiceApplication.class, args);
+	}
+
+}
