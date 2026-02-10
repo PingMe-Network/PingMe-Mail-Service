af2114f742e576ba0b6f75d21b13503bd3324661
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-10 12:56:29 +0700

    trigger redeploy

--- src/main/java/phatdang/ping_me/PingMeMailServiceApplication.java
+++ src/main/java/phatdang/ping_me/PingMeMailServiceApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PingMeMailServiceApplication {
-
-    // Mail Service
+    
     public static void main(String[] args) {
         SpringApplication.run(PingMeMailServiceApplication.class, args);
     }
